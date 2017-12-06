8bf47c20b0e9a7a63e4bf707b0dfd1e6d5fd0e98
ShadmanKazi <skazi@stuy.edu>
2017-12-05 23:11:48 -0500

    added Assassin protagonist

--- v2/Assassin.java
+++ v2/Assassin.java
@@ -1,22 +1,6 @@
 public class Assassin extends Protagonist{
-  private String name;
-
   public Assasin(String name){
     super(name);
     attackRating += 0.5;
   }
 
-  public String getName(){
-    return name;
-  }
-
-  public void specialize(){
-    defense = 20;
-    attackRating = .8;
-  }
-
-  public void normalize(){
-    defense = 40;
-    attackRating = .4;
-  }
-}
